984a07b44fd8be0da6c3cabdae690ed3e796c7a7
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2023-11-16 11:37:41 +0700

    final commit for tutorial 8 django website

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main",
+    "authentication",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,8 +51,14 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 ROOT_URLCONF = "shopping_list.urls"
 
 TEMPLATES = [
